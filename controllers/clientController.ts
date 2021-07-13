a417efa5f77f6f7023cdb050c5916a198214b5d9
wesleygado <wesleygado@gmail.com>
2021-07-13 16:05:14 -0300

    11-trabalhando com views dinamicas

--- controllers/clientController.ts
+++ controllers/clientController.ts
@@ -4,4 +4,23 @@ function index(req: Request, res: Response, next: any){
     res.render('index');
 }
 
-export default { index };
+function produto(req: Request, res: Response, next: any){
+    var produtos = [
+        {
+            nome: 'Memória Ram',
+            descricao: '16Gb'
+        },
+        {
+            nome: 'Placa de video',
+            descricao: '8Gb'
+        },
+        {
+            nome: 'Monitor',
+            descricao: 'FullHD'
+        }
+    ];
+    var produto = produtos[parseInt(`${req.params.id}`)];
+    res.render('produto', {produto: produto});
+}
+
+export default { index, produto };
--- routes/clients.ts
+++ routes/clients.ts
@@ -4,5 +4,6 @@ import clientsController from '../controllers/clientController';
 const router = Router();
 
 router.get('/', clientsController.index);
+router.get('/produto/:id', clientsController.produto);
 
 export default router;
