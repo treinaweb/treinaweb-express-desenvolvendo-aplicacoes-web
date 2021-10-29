d574fdfa0dedcce408b95601899c71b977d4798b
Wesley Gado <wesleygado@gmail.com>
2021-10-29 16:56:32 -0300

    Projeto Final

--- app.ts
+++ app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import clientsRouter from './routes/clients';
+import methodOverride from 'method-override';
 import db from './db';
 
 const app = express();
 const port = parseInt(`${process.env.PORT}`);
 
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 app.use(clientsRouter);
 app.set('view engine', 'pug');
 app.set('views', './views');
--- routes/clients.ts
+++ routes/clients.ts
@@ -3,7 +3,13 @@ import clientsController from '../controllers/clientController';
 
 const router = Router();
 
+router.get('/', clientsController.home);
 router.get('/clients', clientsController.index);
+router.get('/clients/create', clientsController.create);
+router.post('/clients', clientsController.store);
 router.get('/clients/:id', clientsController.show);
+router.put('/clients/:id', clientsController.update);
+router.get('/clients/:id/edit', clientsController.edit);
+router.delete('/clients/:id', clientsController.destroy);
 
 export default router;
